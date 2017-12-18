593455edf4eba884fce73fb8e835695ccb85b48c
tejasrig <tejasrig@gmail.com>
2017-12-18 15:52:19 -0800

    Changes made after the first review

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Logs-Analysis-Project
-This is created to upload the code for Logs Analysis Project in Udacity
+This is created to upload the code for Logs Analysis Project in Udacity. 
+This report is being extracted based on the data from the below tables. 
+The database includes three tables:
+
+The `authors` table includes information about the authors of articles.
+The `articles` table includes the articles themselves.
+The `log` table includes one entry for each time a user has accessed the site.
 
 # Files
 This Project consists of three files that you will see in the repository. 
@@ -15,5 +21,8 @@ _This file has the sample output returned one executing the Source.py_
 * Use Github to fork and clone this [Repo](https://github.com/udacity/fullstack-nanodegree-vm) for VM configuration
 * Start Virtual Machine with these commands 'vagrant.up' and 'vagrant.ssh'
 * Download the data from [here](https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip) for importing the necessary tables and data.
+You will need to unzip this file after downloading it. The file inside is called newsdata.sql. Put this file into the vagrant directory, which is shared with your virtual machine.
+To load the data, cd into the vagrant directory and use the command psql -d news -f newsdata.sql
+Running this command will connect to your installed database server and execute the SQL commands in the downloaded file, creating tables and populating them with data.
 * Check this [link](https://classroom.udacity.com/nanodegrees/nd004/parts/8d3e23e1-9ab6-47eb-b4f3-d5dc7ef27bf0/modules/bc51d967-cb21-46f4-90ea-caf73439dc59/lessons/262a84d7-86dc-487d-98f9-648aa7ca5a0f/concepts/a9cf98c8-0325-4c68-b972-58d5957f1a91) for more info on how to complete the setup
 * After the setup is done, Run the `Source.py` python file in the command line. This will bring up the results. 
--- Source.py
+++ Source.py
@@ -4,29 +4,35 @@
 import psycopg2
 
 
-dname = "news"
-
-try:
-    db = psycopg2.connect('dbname=news')
-    c = db.cursor()
-except:
-    print "Unable to connect to the database"
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        c = db.cursor()
+        return db, c
+    except:
+        print("Unable to connect to the database")
 
 
 # Class that has the main queries to fetch the Logs Analysis Data
 def main():
 
+    db, c = connect()
     # Query 1 to fetch the most popular three articles
     print 'Most popular three articles of all time:'
-    c.execute("select b.title,a.artcount from (select path,count(*) as artcount from log group by path order by artcount desc,path) as a, articles b where a.path like '%'||b.slug||'%' limit 3;")  # noqa
+    c.execute("select title, count(*) as views from log join articles on "
+        "log.path = concat('/article/', articles.slug) group by title "
+        "order by views desc limit 3;")  # noqa
     query1 = c.fetchall()
     for i in range(len(query1)):
         print '\t', query1[i][0], '-', query1[i][1], 'views'
     print ''
 
     # Query 2 to fetch the most popular article authors
-    print 'Most popular article authors of all time:'
-    c.execute("select A.name as AuthorName,sum(B.Views) as ViewCount FROM AUTHORS A, (select b.title as ArticleTitle,b.slug as SlugID, b.author as AuthorID,a.artcount as Views from (select path,count(*) as artcount from log group by path order by artcount desc, path) as a, articles b where a.path like '%'||b.slug||'%') as B where A.id=B.AuthorID group by A.name order by ViewCount desc;")  # noqa
+    print 'Most popular article authors of all time-'
+    c.execute("select authors.name, count(*) as views from authors, articles,"
+        " log where authors.id = articles.author and log.path = "
+        "concat('/article/', articles.slug) group by authors.name "
+        "order by views desc;")  # noqa
     query2 = c.fetchall()
     for i in range(len(query2)):
         print '\t', query2[i][0], '-', query2[i][1], 'views'
@@ -34,7 +40,13 @@ def main():
 
     # Query 3 to fetch the days with more than 1% of requests lead to errors
     print 'Days on which more than 1% of requests lead to errors:'
-    c.execute("select Day, Percentage from (select a.day, (cast((100*a.FailReq) as integer)/cast(b.TotalReq as integer)::float) as Percentage from (select to_char(time,'YYYY-MM-DD') as day,status,COUNT(*) as FailReq from log where status ='404 NOT FOUND' group by day,status) a, (select to_char(time,'YYYY-MM-DD') as day,COUNT(*) as TotalReq from log group by day) b  where to_date(a.day,'YYYY-MM-DD') = to_date(b.day,'YYYY-MM-DD')) as Percent where Percentage > 1.0")  # noqa
+    c.execute("select a.day, ((100*a.FailReq)/(b.TotalReq)::float) from "
+        "(select to_char(time,'YYYY-MM-DD') as day,status,count(*) as FailReq"
+        " from log where status ='404 NOT FOUND' group by day,status) a, "
+        "(select to_char(time,'YYYY-MM-DD') as day,count(*) as TotalReq "
+        "from log group by day) b where to_date(a.day,'YYYY-MM-DD') = "
+        "to_date(b.day,'YYYY-MM-DD') and "
+        "((100*a.FailReq)/(b.TotalReq)::float) > 1")  # noqa
     query3 = c.fetchall()
     for i in range(len(query3)):
         print '\t', query3[i][0], '-', query3[i][1], '%', 'error'
