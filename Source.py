d3db6707419a73f9a8f4256fd9da3082d2757056
tejasrig <tejasrig@gmail.com>
2017-12-19 00:04:16 -0800

    Changes made after review

--- Source.py
+++ Source.py
@@ -1,16 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Importing necessary modules for database integration.
 import psycopg2
 
 
 def connect(database_name="news"):
+    """Funtion to initiate a database connection"""
     try:
         db = psycopg2.connect("dbname={}".format(database_name))
         c = db.cursor()
         return db, c
-    except:
-        print("Unable to connect to the database")
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
 
 # Class that has the main queries to fetch the Logs Analysis Data
@@ -19,37 +20,47 @@ def main():
     db, c = connect()
     # Query 1 to fetch the most popular three articles
     print 'Most popular three articles of all time:'
-    c.execute("select title, count(*) as views from log join articles on "
-        "log.path = concat('/article/', articles.slug) group by title "
-        "order by views desc limit 3;")  # noqa
+    c.execute("""SELECT title, count(*) AS views
+                 FROM log JOIN articles
+                 ON log.path = concat('/article/', articles.slug)
+                 GROUP BY title
+                 ORDER BY views DESC
+                 LIMIT 3;""")
     query1 = c.fetchall()
-    for i in range(len(query1)):
-        print '\t', query1[i][0], '-', query1[i][1], 'views'
+    for title, views in (query1):
+        print '\t', title, '-', views, 'views'
     print ''
 
     # Query 2 to fetch the most popular article authors
     print 'Most popular article authors of all time-'
-    c.execute("select authors.name, count(*) as views from authors, articles,"
-        " log where authors.id = articles.author and log.path = "
-        "concat('/article/', articles.slug) group by authors.name "
-        "order by views desc;")  # noqa
+    c.execute("""SELECT authors.name AS authorname, count(*) AS views
+                 FROM authors, articles, log
+                 WHERE authors.id = articles.author
+                 AND log.path = concat('/article/', articles.slug)
+                 GROUP BY authors.name
+                 ORDER BY views DESC;""")
     query2 = c.fetchall()
-    for i in range(len(query2)):
-        print '\t', query2[i][0], '-', query2[i][1], 'views'
+    for authorname, views in (query2):
+        print '\t', authorname, '-', views, 'views'
     print ''
 
     # Query 3 to fetch the days with more than 1% of requests lead to errors
     print 'Days on which more than 1% of requests lead to errors:'
-    c.execute("select a.day, ((100*a.FailReq)/(b.TotalReq)::float) from "
-        "(select to_char(time,'YYYY-MM-DD') as day,status,count(*) as FailReq"
-        " from log where status ='404 NOT FOUND' group by day,status) a, "
-        "(select to_char(time,'YYYY-MM-DD') as day,count(*) as TotalReq "
-        "from log group by day) b where to_date(a.day,'YYYY-MM-DD') = "
-        "to_date(b.day,'YYYY-MM-DD') and "
-        "((100*a.FailReq)/(b.TotalReq)::float) > 1")  # noqa
+    c.execute("""SELECT to_char(a.day, 'FMMonth DD, YYYY') AS day,
+                 round((100*a.FailReq)/(b.TotalReq)::decimal, 2)
+                 AS percentage
+                 FROM (SELECT time::date AS day,status,count(*) AS FailReq
+                 FROM log
+                 WHERE status ='404 NOT FOUND'
+                 GROUP BY day,status) a,
+                 (SELECT time::date AS day,count(*) AS TotalReq
+                 FROM log
+                 GROUP BY day) b
+                 WHERE a.day = b.day
+                 AND round((100*a.FailReq)/(b.TotalReq)::decimal, 2) > 1;""")
     query3 = c.fetchall()
-    for i in range(len(query3)):
-        print '\t', query3[i][0], '-', query3[i][1], '%', 'error'
+    for day, percentage in (query3):
+        print '\t', day, '-', percentage, '%', 'error'
     print ''
     db.close()
 
